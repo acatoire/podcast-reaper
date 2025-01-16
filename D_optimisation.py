3ff131308bfe40e1f3285b0b152e7837fcfdc185
Axel CATOIRE <axel.catoire@gmail.com>
2025-01-16 21:53:01 +0100

    Start dict work WIP

--- D_optimisation.py
+++ D_optimisation.py
@@ -2,13 +2,9 @@
 This script reads all files in a given directory and its subdirectories,
 concatenates their content, and creates a PDF file with the content of the chosen extension files.
 """
-import locale
+import json
 import os
 import glob
-from datetime import datetime
-
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
 import re
 
 
@@ -21,54 +17,67 @@ class OptimTool:
                  output_path,
                  model_name):
 
-        # Pdf settings and variables
-        self.pdf_canvas = None
-        self.width, self.height = letter
-        self.y_position = self.height - 30
-        self.page_number = 1
-
-
         self.hide_timestamps = True
 
         self.directory_input = input_path
         self.output_dir = output_path
         self.model = model_name
 
+        self.file_dict = {}
+
     def execute(self,
                 ext,
-                folder,
+                folder_ep,
                 with_diari=False,
                 with_merge=False):
 
-        self.reset_canvas()
-
         print("\n\n----------------------------------------")
-
-        # Create the PDF file handler
-        output_pdf = f"{self.output_dir}/{self.model}-{ext}/{self.model}-{folder}-{ext}.txt"
-
-        print(f"Optimization for {folder} with {ext} extension.")
-        os.makedirs(os.path.dirname(output_pdf), exist_ok=True)
-        self.pdf_canvas = canvas.Canvas(output_pdf, pagesize=letter)
+        print(f"Optimization for {folder_ep} with {ext} extension.")
 
         # Find all files in the given directory and its subdirectories
-        # TODO add exclude list
-        files = self.find_files(ext,
-                                contains_txt)
+        files = self.find_files(folder_ep, ext)
 
+        if len(files) != 1:
+            raise ValueError(f"Only one file expected in the folder {folder_ep} with extension {ext}.")
 
-        # Create the PDF file
-        self.create_pdf(files,
-                        with_diari)
-        print(f"PDF created: {output_pdf}")
+        output_file = f"{self.output_dir}/{self.model}-{ext}/{self.model}-{folder_ep}-{ext}"
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+        # Read file content
+        if ext == 'json':
+            file_content = json.loads(self.read_file(files[0])) # FIXME store start and end as miliseconds
+            file_content.pop('text')
+            file_content = file_content['segments']
+        else:
+            # TODO see for others extensions
+            raise ValueError(f"Extension {ext} not supported.")
+
+        # Read diarization speaker as dict
+        if with_diari:
+            diarization_file = os.path.join('transcript',
+                                            'diarization',
+                                            f'{folder_ep}-diari.txt')
+            diarization_speaker = self.get_speaker_info(diarization_file)
+
+            if with_merge:
+                for line in file_content:
+                    print(f"DEBUG : line - {line}")
+                    speaker = self.find_speaker(line['start'], line['end'], diarization_speaker)
+                    line['speaker'] = speaker
+
+        # store new file
+        with open(output_file, 'w') as file_handler:
+            json.dump(file_content, file_handler)
 
     def find_files(self,
-                   ext,
-                   contains_txt):
+                   sub_folder,
+                   ext):
 
         print("Find all .vtt files in the given directory and its subdirectories.")
-        ext_files = glob.glob(os.path.join(self.directory_input, '**',
-                                           f'*{contains_txt}*.{ext}'),
+        ext_files = glob.glob(os.path.join(self.directory_input,
+                                           sub_folder,
+                                           '**',
+                                           f'*.{ext}'),
                               recursive=True)
 
         print(f"Found {len(ext_files)} files in {self.directory_input}:")
@@ -113,7 +122,9 @@ class OptimTool:
         match = re.match(r'start=(\d+\.\d+)s stop=(\d+\.\d+)s speaker_(.+)', line)
         if match:
             start, stop, speaker = match.groups()
-            return float(start), float(stop), speaker
+            return {'start': int(float(start)*1000),
+                    'stop': int(float(stop)*1000),
+                    'speaker': speaker}
         return None
 
     @staticmethod
@@ -163,8 +174,7 @@ class OptimTool:
     def get_speaker_info(self, diarization_path):
 
         if not os.path.exists(diarization_path):
-            print(f"Speaker information file not found: {diarization_path}")
-            return None
+            raise FileNotFoundError(f"Speaker information file not found: {diarization_path}")
 
         with open(diarization_path, 'r') as f:
             speaker_def = f.readlines()
@@ -177,142 +187,6 @@ class OptimTool:
                 speaker_info.append(info)
         return speaker_info
 
-    # Pages creation elements
-    def reset_canvas(self):
-        self.pdf_canvas = None
-        self.y_position = self.height - 30
-        self.page_number = 1
-
-    def add_footer(self):
-        self.pdf_canvas.saveState()
-        self.pdf_canvas.setFont("Helvetica", 9)
-
-        self.pdf_canvas.drawString(self.pdf_canvas._pagesize[0] / 2, 20,
-                                   f"Page {self.page_number}")
-
-        self.pdf_canvas.restoreState()
-
-    def add_page_title(self, title):
-        self.pdf_canvas.saveState()
-
-        self.pdf_canvas.setFont("Helvetica-Bold", 12)
-        self.pdf_canvas.drawString(40,
-                                   self.y_position,
-                                   title)
-
-        self.pdf_canvas.restoreState()
-
-    def first_page(self, title, files_list):
-
-        # first page with list of files
-        self.pdf_canvas.setFont("Helvetica-Bold", 14)
-        self.pdf_canvas.drawString(40, self.y_position, title)
-        self.y_position -= 20
-
-        locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-
-        print("Processing files list for the table of content.")
-
-        self.pdf_canvas.setFont("Helvetica", 10)
-        for index, file_path in enumerate(files_list):
-            file_name = os.path.basename(file_path)
-            name_split = file_name.split('-')
-            date_obj = datetime(int(name_split[0]), int(name_split[1]), int(name_split[2]))
-            full_date = date_obj.strftime('%A %d %B')
-
-            episode_name = ' '.join(name_split[3:]).replace(extension, '')
-            line_content = f"{index}] Episode du {full_date} : {episode_name}"
-
-            print(line_content)
-
-            # manage page jump
-            if self.y_position < 40:
-                self.add_footer()
-                self.pdf_canvas.showPage()
-                self.page_number += 1
-                self.y_position = self.height - 40
-
-
-            # Write the file name and details
-            self.pdf_canvas.drawString(40,
-                                       self.y_position,
-                                       line_content)
-
-            self.y_position -= 12
-
-    def create_pdf(self,
-                   files_list,
-                   with_diari = False):
-
-        self.first_page("Lists des épisodes",
-                        files_list)
-
-        # write the total number of episodes
-
-        self.y_position -= 20
-        self.pdf_canvas.drawString(40, self.y_position,
-                     f"Nombres d'épisodes : {len(files_list)}")
-
-        self.add_footer()
-        self.pdf_canvas.showPage()
-        self.page_number += 1
-        self.y_position = self.height - 30
-
-
-        for file_path in files_list:
-            transcript_file_name = os.path.splitext(os.path.basename(file_path))[0]
-            diarization_file_name = f"{transcript_file_name}-diari.txt"
-            diarization_path = os.path.join('transcript', 'diarization', diarization_file_name)
-
-            transcript_content = self.read_file(file_path)
-            if with_diari:
-                speaker_info = self.get_speaker_info(diarization_path)
-            else:
-                speaker_info = None
-
-            print(f"Processing: {file_path}", end='')
-            self.add_page_title(transcript_file_name)
-
-            self.y_position -= 8
-            self.pdf_canvas.drawString(40,
-                                       self.y_position,
-                                       '-' * 150)
-            self.y_position -= 6
-
-            for line in transcript_content.splitlines():
-
-                # print('.', end='')
-                if self.y_position < 40:
-                    self.add_footer()
-                    self.pdf_canvas.showPage()
-                    self.page_number += 1
-                    self.y_position = self.height - 40
-
-                if line in self.DO_NOT_PRINT:
-                    continue
-
-                if line == '':
-                    continue
-
-                if '-->' in line:
-                    self.pdf_canvas.setFont("Helvetica", 5)
-                else:
-                    self.pdf_canvas.setFont("Helvetica", 10)
-
-                #line_to_write = process_line(line)
-                line_to_write = self.process_line(line,
-                                                  speaker_info)
-
-                self.pdf_canvas.drawString(40,
-                                           self.y_position,
-                                           line_to_write)
-                self.y_position -= 12
-
-            print(' done')
-
-        self.add_footer()
-        self.pdf_canvas.save()
-
 
 if __name__ == "__main__":
 
@@ -320,24 +194,18 @@ if __name__ == "__main__":
     #model = "large-v3"
     model = "turbo"
     directory_input = f"transcript/{model}"
-    #contains_list = ['Plutôt_Confinés_']
+    output_dir = "transcript/"
+    
     with_diarization = True
     merge_speaker = True
 
-    extensions = ["vtt",
+    extensions = [#"vtt",
                   #"txt",
                   #'srt',
                   #'tsv',
-                  #'json'
+                  'json'
                   ]
 
-    # output_pdf_path = f"output_{contains}.pdf"
-    # main(directory_input, output_pdf_path, extension, contains)
-
-    output_dir = "transcript/"
-    #if os.path.exists(output_dir):
-    #    import shutil
-    #    shutil.rmtree(output_dir)
     os.makedirs(output_dir, exist_ok=True)
 
     optim_tool = OptimTool(directory_input,
@@ -347,9 +215,9 @@ if __name__ == "__main__":
     # TODO separate folder with diarization addon
     # TODO separate folder with not pdf concat
 
-    file_list = os.listdir(directory_input)
+    folder_list = os.listdir(directory_input)
 
-    for folder in file_list:
+    for folder in folder_list:
         print(f"------ Processing contains: {folder}")
         for extension in extensions:
             print(f"--- Processing extension: {extension}")
