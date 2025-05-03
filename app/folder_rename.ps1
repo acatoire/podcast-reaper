e551609febd3973d0601c588203866dacd556a3a
Axel CATOIRE <axel.catoire@gmail.com>
2025-05-03 23:53:41 +0200

    improve nav

--- app/folder_rename.ps1
+++ app/folder_rename.ps1
@@ -1,7 +1,6 @@
 # Define the target directory
 $targetFolder = "../podcasts/pardon_gpt/turbo"
 
-
 # Get all subfolders in the target directory
 Get-ChildItem -Path $targetFolder -Directory | ForEach-Object {
     # Extract the date prefix from the folder name (first 10 characters)
@@ -9,7 +8,11 @@ Get-ChildItem -Path $targetFolder -Directory | ForEach-Object {
 
     # Construct the new folder name with the date prefix
     $newFolderName = "$datePrefix"
+    echo "Renaming folder: $($_.Name) to $newFolderName"
 
-    # Rename the folder
-    Rename-Item -Path $_.FullName -NewName $newFolderName
+    # Check if the new name is different from the current name
+    if ($_.Name -ne $newFolderName) {
+        # Rename the folder
+        Rename-Item -Path $_.FullName -NewName $newFolderName
+    }
 }
